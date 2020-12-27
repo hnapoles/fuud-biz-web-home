bd726bfcc4bf890ad98499b3a32e004e4277cf13
Hilario Napoles <hilario.napoles@gmail.com>
2020-12-27 15:27:19 -0500

    added header menu

---
+++ components/topMenuLink.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export default ({ href, children }) => {
+  const router = useRouter()
+
+  let className = children.props.className || ''
+
+  if (router.pathname === href) {
+    className = "border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium";
+  }
+  else {
+    className = "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium";
+  }
+
+  return <Link href={href}>{React.cloneElement(children, { className })}</Link>
+}
