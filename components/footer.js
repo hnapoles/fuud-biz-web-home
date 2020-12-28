ae0b55cabc332f7c6d38a63f3e1242bcdbdc9ab6
Hilario Napoles <hilario.napoles@gmail.com>
2020-12-28 00:39:30 -0500

    fix footer issue

--- components/footer.js
+++ components/footer.js
@@ -187,19 +187,3 @@ export default function Footer() {
     
   )
 }
-
-/*
-<footer>
-      <ul className="flex items-center justify-between p-8">
-        
-          {footerLinks.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="no-underline btn-blue">
-                {label}
-              </a>
-            </li>
-          ))}
-        
-      </ul>
-    </footer>
-*/
